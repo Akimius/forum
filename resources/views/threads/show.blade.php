4bd9b285e05df5621b9232e03a779bdc1168cc5b
Akim MI <akim.savchenko@gmail.com>
2019-01-08 23:14:50 +0200

    Initial commit

---
+++ resources/views/threads/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Forum threads</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                            @foreach($threads as $thread)
+
+                                <atricle>
+                                    <h4>
+                                        {{$thread->title}}
+                                    </h4>
+                                    <div class="body">
+                                        {{$thread->body}}
+                                    </div>
+                                </atricle>
+                                <hr>
+
+                            @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
