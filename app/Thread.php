84ffe7fa059b5468fbf1251577be32ce0e986f21
Akim MI <akim@doubledmarketing.com>
2020-04-13 23:20:50 +0300

    ep 46 refactoring

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Events\ThreadHasNewReply;
 use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 
@@ -65,11 +66,7 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions->filter(
-            function ($sub) use ($reply) {
-                return $sub->user_id != $reply->user_id;
-            }
-        )->each->notify($reply);
+        event(new ThreadHasNewReply($this, $reply));
 
         return $reply;
 
